967a49ed50ac6712b8ab77620e9b8ae1821ac421
mbyrd <mbyrd@trhc.com>
2021-08-12 18:09:51 -0400

    Instructions and modal sizes

--- src/components/GuitarChords.tsx
+++ src/components/GuitarChords.tsx
@@ -1,7 +1,9 @@
-import React, {VFC, useState, useEffect, useContext} from "react"
-import {Context} from "../store"
-import UberChordService from "../services/uberChord"
-import ChordTable from "./ChordTable"
+import React, {VFC, useState, useEffect, useContext} from "react";
+import {Context} from "../store";
+import UberChordService from "../services/uberChord";
+import ChordTable from "./ChordTable";
+import TablatureCheck from "./TablatureCheck";
+import ChordSearch from "./ChordSearch";
 
 const GuitarChords: VFC = () => {
   // @ts-ignore
@@ -10,13 +12,15 @@ const GuitarChords: VFC = () => {
   const [chords, setChords] = useState([])
   const [viewAsTabs, setViewAsTabs] = useState(false)
   const [searchValue, setSearchValue] = useState('a')
+  const instructions = useState('Enter uppercase for A, B, C chords etc. ' +
+    'Bb is B flat. Bdim is B diminished.')
 
   const handleChange = (input: string) => {
     setSearchValue(input);
   }
 
   useEffect(() => {
-    UberChordService.fetchChordsLike(searchValue.toUpperCase())
+    UberChordService.fetchChordsLike(searchValue)
       .then(r => {
         setChords(r);
         console.log(r);
@@ -28,27 +32,10 @@ const GuitarChords: VFC = () => {
     <>
       {state.data}
       <h1>Guitar Chords</h1>
+      <div className="chord-instructions">{instructions}</div>
       <div>
-        <input
-          type="text"
-          className="guitar-input"
-          onChange={e => handleChange(e.target.value)}
-          name="chord-input"
-          value={searchValue}
-          placeholder="Find Chords Like"
-        />
-        <div>
-          <label htmlFor="TablatureCheck">
-            View as tabs?
-            <input
-              id="TablatureCheck"
-              name="tablatureCheck"
-              checked={viewAsTabs}
-              onChange={() => setViewAsTabs(!viewAsTabs)}
-              type="checkbox"
-            />
-          </label>
-        </div>
+        <ChordSearch handleChange={handleChange} searchValue={searchValue} />
+        <TablatureCheck viewAsTabs={viewAsTabs} setViewAsTabs={setViewAsTabs} />
         <div className="chords-container flex-container">
           {chords.length > 0 && chords.map((chord, i) => {
             return (
